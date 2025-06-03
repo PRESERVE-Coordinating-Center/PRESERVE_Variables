560c1891977400a7655dbae8f996ddb99ca3be40
Nguyen, Nhat <nguyenn9@chop.edu>
2025-06-02 22:11:54 -0400

    Added codes for chronic dialysis, kt, and eskd cp

---
+++ analytic_code/eskd_cp_phenotype.R
@@ -0,0 +1,137 @@
+require(tidyr)
+require(dbplyr)
+require(dplyr)
+
+
+#' Identify cohort with End-Stage Kidney Disease (ESKD)  
+#' The computable phenotype was developed to query patients ESKD patients (patients who had either undergone kidney chronic dialysis or kidney transplant) 
+#' and was validated against USRDS dataset and a subset of chart review patients. 
+#' The most optimial computable phenotype for identifying ESKD required one code from Group A (more specific) 
+#' or two distinct codes from Group B (more sensitive) and at least five eGFR measurements ≤15 mL/min/1.73m² on different days. 
+#' This algorithm achieved an F1 score of 0.92 (95% CI 0.88-0.95), sensitivity of 94.6% (95% CI 92.8%-96.2%), and PPV of 86.8.7% (95% CI 84.4%-89.3%)
+
+#' @param cohort_tbl Cohort table of patients meeting all PRESERVE inclusiona and exclusion criteria 
+#' @param egfr_tbl pre-computed eGFR table with at least the following columns: person_id, egfr_measurement_date, egfr_value, 
+#' @param min_date_cutoff Date before which visits are excluded
+#' @param max_date_cutoff Date after which visits are excluded
+#' @param eskd_codeset Validated ESKD codeset 
+#' @param max_egfr_val Maximum eGFR value for cohort inclusion
+#' @param num_egfr Minimum number of eGFR measurements for cohort inclusion
+#' @param min_age Minimum age for cohort inclusion
+#' @param max_age Maximum age for cohort inclusion
+#' @param procedure_tbl Procedures table
+#' @param demographic_tbl Demographic table
+#' @param encounter_tbl Encounter table
+#' @param condition_tbl Condition table
+#' 
+get_eskd_cohort <- function(cohort_tbl, 
+                            procedure_tbl = cdm_tbl("procedures"),
+                            demographic_tbl = cdm_tbl("demographic"), 
+                            encounter_tbl = cdm_tbl("encounter"),
+                            condition_tbl = cdm_tbl("condition")
+                            min_date_cutoff,
+                            max_date_cutoff, 
+                            min_age,
+                            max_age,
+                            transplant_codeset = load_codeset("eskd_cp_USRDS"),
+                            num_egfr = 5,
+                            max_egfr_val = 15){
+
+    # patients with any eskd condition & procedure codes within the desired time
+    cohort_tbl <- cohort_tbl %>%
+                        get_dx_px(cohort = .,
+                               procedure_table = procedure_tbl,
+                               px_codeset = chronic_dialysis_codeset) %>%
+                        filter(px_date >= min_date_cutoff,
+                            px_date <= max_date_cutoff) %>%
+                        compute_new()
+
+     # filter for age at first visit
+     cohort_first <- cohort_tbl %>%
+        group_by(patid) %>%
+        slice_min(report_date, n = 1, with_ties = FALSE) %>%
+        ungroup() %>%
+        inner_join(demographic_tbl %>%
+                   select(patid, birth_date), by = "patid") %>%
+        mutate(age_at_ced = as.numeric(difftime(report_date, birth_date, units = "days")) / 365.25) %>%
+        filter(age_at_ced >= min_age,
+               age_at_ced <= max_age) %>%
+        compute_new()
+
+    cohort_tbl <- cohort_tbl %>%
+         inner_join(select(cohort_first, patid), by = "patid") %>%
+         compute_new()
+
+    # get first and last visit dates
+    first_last_visits <- encounter_tbl %>%
+                        inner_join(select(cohort_tbl, patid), by = "patid") %>%
+                        group_by(patid) %>%
+                        summarise(first_visit_date = min(admit_date, na.rm = TRUE),
+                                last_visit_date = max(admit_date, na.rm = TRUE)) %>% ungroup() %>%
+                        compute_new()
+
+    # exclude patients with last vist date before the minimum date cutoff
+    # and patients with first visit date after the maximum date cutoff
+    first_last_visits <- first_last_visits %>%
+       filter(last_visit_date >= min_date_cutoff, 
+              first_visit_date >= max_date_cutoff) %>%
+       compute_new()
+
+    cohort_tbl <- cohort_tbl %>% 
+                inner_join(first_last_visits, by = "patid") %>%
+                compute_new()
+
+    # patients with at least 1 code in group A
+    group_A <- cohort_tbl %>%
+         filter(group == "A") %>%    
+         distinct(patid)
+
+    # patients with at least 2 codes in group B on 2 separate visits
+    # AND at least five eGFR measurements  ≤15 mL/min/1.73m² on different days with no time restrictions
+    group_B <- cohort_tbl %>%
+               filter(group == "B") %>%
+               group_by(patid) %>%
+               summarise(n_px = n_distinct(report_date)) %>% ungroup() %>%
+               filter(n_px >= 2) %>%
+               inner_join(egfr_tbl, by = "patid") %>%
+               compute_new()
+    
+    if (!is.na(max_egfr_val)) {
+       group_B <- group_B %>%
+              filter(egfr_value <= max_egfr_val) %>%
+              group_by(patid) %>%
+              summarise(n_egfr = n_distinct(egfr_measurement_date)) %>% ungroup() %>%
+              filter(n_egfr >= num_egfr) %>% 
+              distinct(patid) %>%
+              compute_new()}
+
+    group_A %>%
+       dplyr::union(group_B) %>%  
+       distinct(patid) %>%
+       compute_new() %>%
+       return()                              
+}
+
+#' Get first date of transplant or post-transplant complication procedures 
+#'
+#' @param cohort Cohort of interest: preserve cohort
+#' @param procedure_table PCORnet procedures table
+#' @param px_codeset Procedure codeset - must contain fields concept_code and pcornet_vocabulary_id
+#'
+#' @return Procedures in codeset
+#'
+get_dx_px <- function(cohort = cdm_tbl("demographic"),
+                      procedure_table = cdm_tbl("procedures"),
+                      condition_table = cdm_tbl("condition"),
+                      dx_codeset, px_codeset) {
+  tbl <- get_dx(cohort, 
+        dx_codeset = dx_codeset) %>% 
+        mutate(type = "condition") %>%
+        select(patid, id = conditionid, encounterid, report_date = report_date, vocabulary_type = condition_type, site, concept_code = condition, type) %>% 
+        union(get_px(cohort,
+                  px_codeset = px_codeset) %>%
+        mutate(type = "procedure") %>% 
+        select(patid, id = proceduresid, encounterid, report_date = admit_date, vocabulary_type = px_type, site, concept_code = px, type), by = c("patid"))# 
+
+    return (tbl)
+}
---
+++ analytic_code/kidney_transplant_cp_USRDS.R
@@ -0,0 +1,180 @@
+
+require(tidyr)
+require(dbplyr)
+require(dplyr)
+
+
+#' Identify cohort with a kidney transplant procedure 
+#' The computable phenotype was developed to query patients who had undergone kidney transplant 
+#' and was validated against USRDS dataset and a subset of chart review patients. 
+#' The most optimal algorithm for identifying children with kidney transplant used one code from Group A (more specific) 
+#' and two distinct codes from Group B (more sensitive), achieving an F1 score of 0.93 (95% CI 0.89-0.97), 
+#' a PPV of 90.7% (95% CI 84.2%-95.8%), and sensitivity of 96.0% (95% CI 91.8%-100.0%). 
+#' 
+#' This function is a wrapper of the get_chronic_dialysis_cohort function with no egfr requirement.
+#' @param cohort_tbl Cohort table of patients meeting all PRESERVE inclusiona and exclusion criteria 
+#' @param egfr_tbl pre-computed eGFR table with at least the following columns: person_id, egfr_measurement_date, egfr_value, 
+#' @param min_date_cutoff Date before which visits are excluded
+#' @param max_date_cutoff Date after which visits are excluded
+#' @param kidney_transplant_codeset Validated chronic dialysis codeset 
+#' @param max_egfr_val Maximum eGFR value for cohort inclusion
+#' @param num_egfr Minimum number of eGFR measurements for cohort inclusion
+#' @param min_age Minimum age for cohort inclusion
+#' @param max_age Maximum age for cohort inclusion
+#' @param procedure_tbl Procedures table
+#' @param demographic_tbl Demographic table
+#' @param encounter_tbl Encounter table
+
+get_kidney_transplant_cohort <- function(cohort_tbl, 
+                                        procedure_tbl = cdm_tbl("procedures"),
+                                        demographic_tbl = cdm_tbl("demographic"), 
+                                        encounter_tbl = cdm_tbl("encounter"),
+                                        min_date_cutoff,
+                                        max_date_cutoff, 
+                                        min_age,
+                                        max_age,
+                                        kidney_transplant_codeset = load_codeset("kidney_transplant_cp_USRDS"),
+                                        num_egfr = 0,
+                                        max_egfr_val = NA){
+
+    get_chronic_dialysis_cohort(cohort_tbl, 
+                                min_date_cutoff,
+                                max_date_cutoff, 
+                                min_age,
+                                max_age,
+                                kidney_transplant_codeset,
+                                num_egfr = num_egfr,
+                                max_egfr_val = num_egfr) %>%
+    return()                            
+}
+
+
+#' Identify cohort with chronic dialysis 
+#' The computable phenotype was developed to query patients who had undergone kidney chronic dialysis and was validated against USRDS dataset and 
+#' a subset of chart review patients. 
+#' The most optimial computable phenotype for identifying chronic dialysis required one code from Group A (more specific) 
+#' or two distinct codes from Group B (more sensitive) and at least five eGFR measurements  ≤15 mL/min/1.73m² on different days with no time restrictions. 
+#' This algorithm achieved an F1 score of 0.85 (95% CI 0.77-0.91), sensitivity of 82.5% (95% CI 72.2%-91.5%), and PPV of 86.7% (95% CI 77.1%-94.1%).
+#' @param cohort_tbl Cohort table of patients meeting all PRESERVE inclusiona and exclusion criteria 
+#' @param egfr_tbl pre-computed eGFR table with at least the following columns: person_id, egfr_measurement_date, egfr_value, 
+#' @param min_date_cutoff Date before which visits are excluded
+#' @param max_date_cutoff Date after which visits are excluded
+#' @param chronic_dialysis_codeset Validated chronic dialysis codeset 
+#' @param max_egfr_val Maximum eGFR value for cohort inclusion
+#' @param num_egfr Minimum number of eGFR measurements for cohort inclusion
+#' @param min_age Minimum age for cohort inclusion
+#' @param max_age Maximum age for cohort inclusion
+#' @param procedure_tbl Procedures table
+#' @param demographic_tbl Demographic table
+#' @param encounter_tbl Encounter table
+
+get_chronic_dialysis_cohort <- function(cohort_tbl, 
+                                        procedure_tbl = cdm_tbl("procedures"),
+                                        demographic_tbl = cdm_tbl("demographic"), 
+                                        encounter_tbl = cdm_tbl("encounter"),
+                                        min_date_cutoff,
+                                        max_date_cutoff, 
+                                        min_age,
+                                        max_age,
+                                        chronic_dialysis_codeset = load_codeset("chronic_dialysis_cp_USRDS"),
+                                        num_egfr = 5,
+                                        max_egfr_val = 15){
+
+     # patients with any chronic dialysis code within the desired time
+     cohort_tbl <- cohort_tbl %>%
+                        get_px(cohort = .,
+                               procedure_table = procedure_tbl,
+                               px_codeset = chronic_dialysis_codeset) %>%
+                        filter(px_date >= min_date_cutoff,
+                            px_date <= max_date_cutoff) %>%
+                        compute_new()
+
+     # filter for age at first visit
+     cohort_first <- cohort_tbl %>%
+        group_by(patid) %>%
+        slice_min(px_date, n = 1, with_ties = FALSE) %>%
+        ungroup() %>%
+        inner_join(demographic_tbl %>%
+                   select(patid, birth_date), by = "patid") %>%
+        mutate(age_at_ced = as.numeric(difftime(px_date, birth_date, units = "days")) / 365.25) %>%
+        filter(age_at_ced >= min_age,
+               age_at_ced <= max_age) %>%
+        compute_new()
+
+    cohort_tbl <- cohort_tbl %>%
+         inner_join(select(cohort_first, patid), by = "patid") %>%
+         compute_new()
+
+    # get first and last visit dates
+    first_last_visits <- encounter_tbl %>%
+                        inner_join(select(cohort_tbl, patid), by = "patid") %>%
+                        group_by(patid) %>%
+                        summarise(first_visit_date = min(admit_date, na.rm = TRUE),
+                                last_visit_date = max(admit_date, na.rm = TRUE)) %>% ungroup() %>%
+                        compute_new()
+
+    # exclude patients with last vist date before the minimum date cutoff
+    # and patients with first visit date after the maximum date cutoff
+    first_last_visits <- first_last_visits %>%
+       filter(last_visit_date >= min_date_cutoff, 
+              first_visit_date >= max_date_cutoff) %>%
+       compute_new()
+
+    cohort_tbl <- cohort_tbl %>% 
+                inner_join(first_last_visits, by = "patid") %>%
+                compute_new()
+
+    # patients with at least 1 code in group A
+    group_A <- cohort_tbl %>%
+         filter(group == "A") %>%    
+         distinct(patid)
+
+    # patients with at least 2 codes in group B on 2 separate visits
+    # AND at least five eGFR measurements  ≤15 mL/min/1.73m² on different days with no time restrictions
+    group_B <- cohort_tbl %>%
+               filter(group == "B") %>%
+               group_by(patid) %>%
+               summarise(n_px = n_distinct(px_date)) %>% ungroup() %>%
+               filter(n_px >= 2) %>%
+               inner_join(egfr_tbl, by = "patid") %>%
+               compute_new()
+    
+    if (!is.na(max_egfr_val)) {
+       group_B <- group_B %>%
+              filter(egfr_value <= max_egfr_val) %>%
+              group_by(patid) %>%
+              summarise(n_egfr = n_distinct(egfr_measurement_date)) %>% ungroup() %>%
+              filter(n_egfr >= num_egfr) %>% 
+              distinct(patid) %>%
+              compute_new()}
+
+    group_A %>%
+       dplyr::union(group_B) %>%  
+       distinct(patid) %>%
+       compute_new() %>%
+       return()                              
+}
+
+#' Get procedures in codeset
+#'
+#' @param cohort Cohort of interest: preserve cohort
+#' @param procedure_table PCORnet procedures table
+#' @param px_codeset Procedure codeset - must contain fields concept_code and pcornet_vocabulary_id
+#'
+#' @return Procedures in codeset
+#'
+get_px <- function(cohort = cdm_tbl("demographic"),
+                   procedure_table = cdm_tbl("procedures"),
+                   px_codeset) {
+  tbl <- procedure_table %>%
+    inner_join(select(cohort, patid), by = "patid") 
+  if("type" %in% colnames(px_codeset)){
+    tbl <- tbl %>% inner_join(select(px_codeset, concept_code, pcornet_vocabulary_id, type),
+               by = c("px" = "concept_code",
+                      "px_type" = "pcornet_vocabulary_id"))
+  } else {
+    tbl <- tbl %>% inner_join(select(px_codeset, concept_code, pcornet_vocabulary_id),
+               by = c("px" = "concept_code",
+                      "px_type" = "pcornet_vocabulary_id"))
+  }
+}
